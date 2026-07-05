2d160d393a513fce320b81d7774d5a04d0cada17
SK8-infi <shivansh.katiyar1712@gmail.com>
2026-07-06 03:00:18 +0530

    Remove Microsoft CMT acknowledgment from data files

--- src/data/siteConfig.js
+++ src/data/siteConfig.js
@@ -39,7 +39,5 @@ export const siteConfig = {
   },
 
   // Footer acknowledgments (e.g., CMT disclaimer)
-  acknowledgments: [
-    'The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.'
-  ],
+  acknowledgments: [],
 };
